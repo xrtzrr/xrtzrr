b37ef0665cdd89f4d9c3679fd9d3fb833feddfcf
paw <xDarkWasTaken001@gmail.com>
2022-11-16 21:40:23 -0700

    Create README.lua

---
+++ README.lua
@@ -0,0 +1,7 @@
+# -- Hi, My name is Pawstep!
+local workingon = [ "palera1n", "Permasigner", "Zyno", "itsnebula.net" ]
+local favoriteprogramlanguages = [ "LUA", "Python" ]
+local learning = [ "Python" ]
+
+# -- Please encrypt your emails if necessary
+    local email = "5dkyQHivc7zYxjCmN4qAMH7qtkfPn8@proton.me"
