c5761e7e93be1fba2776ffec507b5e19a6bd162f
paw <xDarkWasTaken001@gmail.com>
2022-12-10 11:43:37 -0700

    Update and rename README.md to README.lua

--- README.lua
+++ README.lua
@@ -6,4 +6,5 @@ local learningrightnow = ("Python")
 -- Please encrypt your emails if needed.
 local email = ("5dkyQHivc7zYxjCmN4qAMH7qtkfPn8@proton.me")
 
-Profile Views:   [![HitCount](https://hits.dwyl.com/Pawstep/Pawstep.svg?style=flat-square)](http://hits.dwyl.com/Pawstep/Pawstep)
+Profile Views:
+[![HitCount](https://hits.dwyl.com/Pawstep/Pawstep.svg?style=flat-square)](http://hits.dwyl.com/Pawstep/Pawstep)
