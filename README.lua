b456616b7c4f30768ea30329a1c075d39b57983c
paw <xDarkWasTaken001@gmail.com>
2022-12-10 11:37:38 -0700

    Update README.lua

--- README.lua
+++ README.lua
@@ -5,3 +5,5 @@ local learningrightnow = ("Python")
 
 -- Please encrypt your emails if needed.
 local email = ("5dkyQHivc7zYxjCmN4qAMH7qtkfPn8@proton.me")
+
+![image title](https://rushter.com/counter.svg)
